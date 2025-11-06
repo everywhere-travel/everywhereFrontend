233e8a6643878ca509751bb7a9ade5260d20e9dd
jaime <jpalominoc5@upao.edu.pe>
2025-11-06 01:15:55 -0500

    feat: adding the base component

---
+++ everywhere-frontend/src/app/pages/detalle-juridico/detalle-juridico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-juridico',
+  templateUrl: './detalle-juridico.component.html',
+  styleUrls: ['./detalle-juridico.component.css']
+})
+export class DetalleJuridicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
