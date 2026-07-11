f3f7ecd98e399761da0120525ad1e5b7e08c0ef5
jaime <109822463+RpC05@users.noreply.github.com.>
2026-07-11 01:30:22 -0500

    feat: adding unit test

--- everywhere-frontend/src/app/pages/detalle-cotizacion/detalle-cotizacion.component.spec.ts
+++ everywhere-frontend/src/app/pages/detalle-cotizacion/detalle-cotizacion.component.spec.ts
@@ -3,17 +3,17 @@ import { HttpClientTestingModule, HttpTestingController, provideHttpClientTestin
 import { provideHttpClient } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { DetallePersonaComponent } from './detalle-persona.component';
+import { DetalleCotizacionComponent } from './detalle-cotizacion.component';
 
-describe('DetallePersonaComponent', () => {
-  let component: DetallePersonaComponent;
-  let fixture: ComponentFixture<DetallePersonaComponent>;
+describe('DetalleCotizacionComponent', () => {
+  let component: DetalleCotizacionComponent;
+  let fixture: ComponentFixture<DetalleCotizacionComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [provideHttpClient(), provideHttpClientTesting()],
       imports: [
-        DetallePersonaComponent, // Para standalone components
+        DetalleCotizacionComponent, // Para standalone components
         HttpClientTestingModule,
         RouterTestingModule,
         NoopAnimationsModule
@@ -21,7 +21,7 @@ describe('DetallePersonaComponent', () => {
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(DetallePersonaComponent);
+    fixture = TestBed.createComponent(DetalleCotizacionComponent);
     component = fixture.componentInstance;
     // Evitamos llamar a detectChanges inmediatamente para no detonar llamadas a servicios reales sin mockear
     // fixture.detectChanges();
