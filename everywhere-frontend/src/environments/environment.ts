d988d3196489154482f19c04f897f0acea24e379
RpC05 <109822463+RpC05@users.noreply.github.com>
2026-08-10 17:44:06 -0500

    Update baseURL in environment configuration

--- everywhere-frontend/src/environments/environment.ts
+++ everywhere-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseURL: "https://everywherebackend-p64d.onrender.com/api/v1"
+    baseURL: "https://everywherebackend.onrender.com/api/v1"
 };
